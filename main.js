24ff86e2b96da3d7d27ddd84a9b50cc0e9225892
Kamil Naja <kamilnaja@gmail.com>
2017-05-21 18:45:12 +0200

    added dynamical object with watches

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
-    document.querySelector('.hamburger').addEventListener('click', function() {
-        document.querySelector('.navbar').classList.toggle('mobile');
+document.addEventListener("DOMContentLoaded", function(){
+    // Handler when the DOM is fully loaded
+
+    //alias for qsa
+    function $(el) {
+        return document.querySelector(el);
+    }
+    $('.hamburger').addEventListener('click', function() {
+        $('.navbar').classList.toggle('mobile');
     });
+
+    var app1 = new Vue ({
+        el: '#watches-list',
+        data: {
+            watches: [
+                {
+                    name: 'Brajbling',
+                    photo: 'img/braitling.png',
+                    price: Math.floor(Math.random() * 1000) + ".99$"
+                },
+                {
+                    name: 'Rolnix',
+                    photo: 'img/rolnix.png',
+                    price: Math.floor(Math.random() * 1000) + ".99$"
+                },
+                {
+                    name: 'Tag hajer',
+                    photo: 'img/hauer.png',
+                    price: Math.floor(Math.random() * 1000) + ".99$"
+                },
+                {
+                    name: 'Lamaga 007',
+                    photo: 'img/omega.png',
+                    price: Math.floor(Math.random() * 1000) + ".99$"
+                },
+                {
+                    name: 'Patyk Filip',
+                    photo: 'img/patek.png',
+                    price: Math.floor(Math.random() * 1000) + ".99$"
+                }
+
+            ],
+        }
+    })
+});
+
+
+
+
